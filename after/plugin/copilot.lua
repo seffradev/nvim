66ac3ec11ca831751bb3b98aa343714e1626799c
Hampus Avekvist <hampus.avekvist@outlook.com>
2023-06-18 13:17:03 +0200

    feat: added copilot bindings

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>cp", "<cmd>Copilot panel<CR>", { desc = "Toggle Copilot Panel" })
+vim.keymap.set("n", "<leader>cs", "<cmd>Copilot status<CR>", { desc = "Show Copilot Status" })
