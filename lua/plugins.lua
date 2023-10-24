e46eae13a96f8cefd00326330ed918f6bc0f3720
Hampus Avekvist <ha@leissner.se>
2023-10-24 15:34:13 +0200

    feat: added chatgpt support

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -82,4 +82,13 @@ return {
     {
         "mfussenegger/nvim-jdtls",
     },
+    {
+        "jackMort/ChatGPT.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+    },
 }
